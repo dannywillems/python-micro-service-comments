c4c61ec8b416699a5606a47725461b77e12e895c
Danny Willems <contact@danny-willems.be>
2017-07-15 22:37:52 +0200

    First version with get all comments.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # python-micro-service-comments
+
 A micro service to manage comments written in Python.
+
+## Dependencies.
+
+- flask
+- sqlite3
+
+## Use with Docker
+
+```
+docker run --name micro-service-comments -p 5000:5000 dannywillems/python-micro-service-comments
+```
---
+++ src/main.py
@@ -0,0 +1,55 @@
+import flask
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+import json
+
+
+Base = declarative_base()
+
+
+class Engine:
+    def __init__(self):
+        self.engine = sqlalchemy.create_engine("sqlite:///comment.sqlite", echo=True)
+        self.Session = sqlalchemy.orm.sessionmaker(bind=self.engine)
+        self.session = self.Session()
+
+    def add(self, obj):
+        self.session.add(obj)
+
+    def commit(self):
+        self.session.commit()
+
+class Comment(Base):
+    __tablename__ = "comment"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    article_id = sqlalchemy.Column(sqlalchemy.Integer)
+    text = sqlalchemy.Column(sqlalchemy.String)
+    datetime = sqlalchemy.Column(sqlalchemy.DateTime)
+    moderated = sqlalchemy.Column(sqlalchemy.Boolean)
+    # parent_id = sqlalchemy.Column(sqlalchemy.Integer)
+
+    def json_of_comment(self):
+        d = {
+            "id": self.id,
+            "article_id": self.article_id,
+            "text": self.text,
+            "datetime": self.datetime.strftime("%Y-%m-%d %H:%M:%S"),
+            "moderated": self.moderated
+        }
+        return d
+
+app = flask.Flask(__name__)
+
+e = Engine()
+# t = datetime.datetime.now()
+# test = Comment(article_id=0, text="hello", datetime=t, moderated=False)
+# e.add(test)
+# e.commit()
+
+@app.route("/comment")
+def get_all_comments():
+    # print e.query(Comment).all()
+    all_comments = e.session.query(Comment).all()
+    return json.dumps([comment.json_of_comment() for comment in all_comments])
